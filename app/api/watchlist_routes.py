b2442558efa4e23cde0f423bdb04141e5ac73115
grassella <negrete.graciela@yahoo.com>
2025-04-08 20:46:29 -0500

    finished watchlist.py model

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, request, jsonify
-from app.models import db, Watchlist
+from flask_login import login_required, current_user
+from app.models import db, Watchlist, Stock
 
 watchlists_routes = Blueprint('watchlists', __name__)
 
-@watchlists_routes.route('/', methods=['GET'])
+@watchlists_routes.route('/api/watchlist', methods=['GET'])
 def view_watched_stocks():
     watchlists = Watchlist.query.all()
     return jsonify([watchlist.to_dict() for watchlist in watchlists])
 
-@watchlists_routes.route('/add', methods=['POST'])
+@watchlists_routes.route('/api/watchlist', methods=['POST'])
 def add_stock():
+    stock_id = request.json.get('stock_id')
+    stock = Stock.query.get(stock_id)
 
-@watchlists_routes.route('/remove', methods=['DELETE'])
+@watchlists_routes.route('/api/watchlist/:stock_id', methods=['DELETE'])
 def remove_stock():
--- app/models/watchlist.py
+++ app/models/watchlist.py
@@ -2,9 +2,36 @@ from .db import db
 
 class Watchlist(db.Model):
     __tablename__ = 'watchlist'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    stock_id = db.Column(db.Integer, db.ForeignKey('stocks.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stocks.id'), nullable = False)
 
-    user = db.relationship('User', back_populates='watchlist')
-    stock = db.relationship('Stock', back_populates='watchlists')
+    user = db.relationship('User', back_populates = 'watchlist')
+    stock = db.relationship('Stock', back_populates = 'watchlists')
+
+class WatchlistStock(db.Model):
+    __tablename__ = 'watchlist_stocks'
+    id = db.Column(db.Integer, primary_key = True)
+    watchlist_id = db.Column(db.integer, db.ForeignKey('watchlist_id'), nullable = False)
+    stock_id = db.Column(db.integer, db.ForeignKey('stock_id'), nullable = False)
+
+    watchlist = db.relationship('Watchlist', back_populates = 'stocks')
+    stock = db.relationship('Stock', back_populates = 'watchlist_entries')
+
+class Stock(db.Model):
+    __tablename__='stocks'
+    id = db.Column(db.Integer, primary_key = True)
+    symbol = db.Column(db.String, nullable = False, unique = True)
+    name = db.Column(db.String, nullable = False)
+
+    Watchlist_entries = db.relationship('WatchlistStock', back_populates = 'stock')
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Ineteger, primary_key = True)
+    username = db.Column(db.String, nullable = False, unique = True)
+
+    watchlist = db.relationship('Watchlist', uselist = False, back_populates = 'user', cascade = 'all, delete-orphan')
+
+
+    
